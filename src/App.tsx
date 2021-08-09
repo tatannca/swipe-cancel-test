c7bfeaf75e88e2b54d4070e4c150c3051d826d62
tateno <gd28.c.tateno@gmail.com>
2021-08-09 14:44:37 +0900

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { css } from '@emotion/react';
 
+const swipeControlRange = 30;
 const swipeTitle = css`
   font-size: 20px;
   text-align: center;
-`;
-const swipePossible = css`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: orange;
-  color: #fff;
-  padding: 100px 0;
-  font-weight: bold;
 `;
 const swipeImpossible = css`
   display: flex;
@@ -22,15 +14,57 @@ const swipeImpossible = css`
   color: #fff;
   padding: 100px 0;
   font-weight: bold;
+  position: relative;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: ${swipeControlRange}px;
+    height: 100%;
+    background: coral;
+  }
+  &:after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: ${swipeControlRange}px;
+    height: 100%;
+    background: coral;
+  }
 `;
 
 function App() {
 
+  const container = useRef<HTMLDivElement>(null);
+
+  const touchStartEvent = (e: any) => {
+    if (e.pageX > 30 && e.pageX < window.innerWidth - 30) return;
+    e.preventDefault();
+    console.log(`touch start ${e.pageX}`);
+  }
+
+  // useEffect(() => {
+  //   if (container.current) {
+  //     container.current.addEventListener('touchstart', (e) => touchStartEvent(e));
+  //   }
+
+  //   return () => {
+  //     if (container.current) {
+  //       container.current.removeEventListener('touchstart', touchStartEvent);
+  //     }
+  //   }
+  // }, [container.current]);
+
   return (
-    <div>
+    <div
+      ref={container}
+      onTouchStart={touchStartEvent}
+      onClick={touchStartEvent}
+    >
       <h1 css={swipeTitle}>スワイプキャンセルテスト</h1>
-      <div css={swipePossible}>スワイプ可能範囲</div>
-      <div css={swipeImpossible}>スワイプ不可範囲</div>
+      <div css={swipeImpossible}>スワイプ不可ブロック</div>
     </div>
   )
 }
