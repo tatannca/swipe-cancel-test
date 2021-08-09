337fa877bb4e57bf677afa514be0f86c484a8343
tateno <gd28.c.tateno@gmail.com>
2021-08-09 13:21:05 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { css } from '@emotion/react';
+
+function App() {
+
+  return (
+    <div>
+      <div>スワイプキャンセルテスト</div>
+    </div>
+  )
+}
+
+export default App
