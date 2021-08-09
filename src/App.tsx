c0a78cf1a0a0586307b091edd23da00d937d1b58
tateno <gd28.c.tateno@gmail.com>
2021-08-09 13:30:54 +0900

    スタイル調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
 import { css } from '@emotion/react';
 
+const swipeTitle = css`
+  font-size: 20px;
+  text-align: center;
+`;
+const swipePossible = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: orange;
+  color: #fff;
+  padding: 100px 0;
+  font-weight: bold;
+`;
+const swipeImpossible = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: cadetblue;
+  color: #fff;
+  padding: 100px 0;
+  font-weight: bold;
+`;
+
 function App() {
 
   return (
     <div>
-      <div>スワイプキャンセルテスト</div>
+      <h1 css={swipeTitle}>スワイプキャンセルテスト</h1>
+      <div css={swipePossible}>スワイプ可能範囲</div>
+      <div css={swipeImpossible}>スワイプ不可範囲</div>
     </div>
   )
 }
